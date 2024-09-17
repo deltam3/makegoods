d1f0c1fb5aff94dd68b69b3fc88194dff6219ae3
deltam3 <practicect@proton.me>
2024-09-17 17:05:42 +0900

    움직이는 에코백 section + CTA section 완료

--- makegoods/src/app/page.tsx
+++ makegoods/src/app/page.tsx
@@ -1,8 +1,12 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Bag from "@/components/Bag";
 import Tshirt from "@/components/Tshirt";
-import { Check, Star } from "lucide-react";
+import { ArrowRight, Check, Star } from "lucide-react";
 import { Icons } from "@/components/Icons";
+import { Reviews } from "../components/Reviews";
+import Phone from "../components/Phone";
+import Link from "next/link";
+import { buttonVariants } from "@/components/ui/button";
 
 export default function Home() {
   return (
@@ -256,6 +260,75 @@ export default function Home() {
               </div>
             </div>
           </div>
+        </MaxWidthWrapper>
+        <div className="pt-16">
+          <Reviews />
+        </div>
+      </section>
+
+      <section>
+        <MaxWidthWrapper className="py-24">
+          <div className="mb-12 px-6 lg:px-8">
+            <div className="mx-auto max-w-2xl sm:text-center">
+              <h2 className="order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900">
+                원하는 이미지로{" "}
+                <span className="relative px-2 bg-primary text-white">
+                  당신만의 굿즈를
+                </span>{" "}
+                제작하세요.
+              </h2>
+            </div>
+          </div>
+
+          <div className="mx-auto max-w-6xl px-6 lg:px-8">
+            <div className="relative flex flex-col items-center md:grid grid-cols-2 gap-40">
+              <img
+                src="/arrow2.png"
+                className="absolute top-[25rem] md:top-1/2 -translate-y-1/2 z-10 left-1/2 -translate-x-1/2 rotate-90 md:rotate-0 w-[15%]"
+              />
+
+              <div className="relative h-80 md:h-full w-full md:justify-self-end max-w-sm rounded-xl bg-gray-900/5 ring-inset ring-gray-900/10 lg:rounded-2xl">
+                <img
+                  src="/testimonials/6.jpg"
+                  className="rounded-md object-cover bg-white shadow-2xl ring-1 ring-gray-900/10 h-full w-full"
+                />
+              </div>
+
+              <Bag className="w-60" imgSrc="/testimonials/6.jpg" />
+            </div>
+          </div>
+
+          <ul className="mx-auto mt-12 max-w-prose sm:text-lg space-y-2 w-fit">
+            <li className="w-fit">
+              <Check className="h-5 w-5 text-primary inline mr-1.5" />
+              고급 재질
+            </li>
+            <li className="w-fit">
+              <Check className="h-5 w-5 text-primary inline mr-1.5" />
+              침착맨 싸인 옵션
+            </li>
+            <li className="w-fit">
+              <Check className="h-5 w-5 text-primary inline mr-1.5" />
+              빠른 배송
+            </li>
+            <li className="w-fit">
+              <Check className="h-5 w-5 text-primary inline mr-1.5" />1 년 A/S
+              보장
+            </li>
+
+            <div className="flex justify-center">
+              <Link
+                className={buttonVariants({
+                  size: "lg",
+                  className: "mx-auto mt-8",
+                })}
+                href="/configure/upload"
+              >
+                지금 당장 침착맨 굿즈 제작{" "}
+                <ArrowRight className="h-4 w-4 ml-1.5" />
+              </Link>
+            </div>
+          </ul>
         </MaxWidthWrapper>
       </section>
     </div>
