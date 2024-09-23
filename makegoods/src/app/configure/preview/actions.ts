d9d1b499a0e9c1d20cf813b521c6fc32d0bf1a6e
deltam3 <practicect@proton.me>
2024-09-23 16:57:32 +0900

    encodedRedirect fix

--- makegoods/src/app/configure/preview/actions.ts
+++ makegoods/src/app/configure/preview/actions.ts
@@ -3,6 +3,7 @@
 import { createClient } from "../../../../utils/supabase/server";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
+import { encodedRedirect } from "../../../../utils/utils";
 
 export const signUpAction = async (formData: FormData) => {
   const email = formData.get("email")?.toString();
