bccd4db3a4ab4f8105fb88e3c32375fdf4d599e9
deltam3 <practicect@proton.me>
2024-09-23 16:31:47 +0900

    ununsed login comment out

--- makegoods/src/app/login/page.tsx
+++ makegoods/src/app/login/page.tsx
@@ -1,62 +1,62 @@
-"use client";
-import { FormEvent, useState } from "react";
-import Form from "../../components/custom/Form";
-import Input from "../../components/custom/Input";
-import { Button } from "../../components/ui/button";
-import FormRowVertical from "../../components/custom/FormRowVertical";
-import { useLogin } from "./useLogin";
+// "use client";
+// import { FormEvent, useState } from "react";
+// import Form from "../../components/custom/Form";
+// import Input from "../../components/custom/Input";
+// import { Button } from "../../components/ui/button";
+// import FormRowVertical from "../../components/custom/FormRowVertical";
+// import { useLogin } from "./useLogin";
 
-export default function Page() {
-  const [email, setEmail] = useState<string>("example@g.com");
-  const [password, setPassword] = useState<string>("password");
-  const { login, isPending } = useLogin();
+// export default function Page() {
+//   const [email, setEmail] = useState<string>("example@g.com");
+//   const [password, setPassword] = useState<string>("password");
+//   const { login, isPending } = useLogin();
 
-  function handleSubmit(e: FormEvent<HTMLFormElement>) {
-    e.preventDefault();
-    if (!email || !password) return;
-    login(
-      { email, password },
-      {
-        onSettled: () => {
-          setEmail("");
-          setPassword("");
-        },
-      }
-    );
-  }
+//   function handleSubmit(e: FormEvent<HTMLFormElement>) {
+//     e.preventDefault();
+//     if (!email || !password) return;
+//     login(
+//       { email, password },
+//       {
+//         onSettled: () => {
+//           setEmail("");
+//           setPassword("");
+//         },
+//       }
+//     );
+//   }
 
-  return (
-    <div className="grid grid-cols-[48rem] content-center justify-center gap-[3.2rem] bg-grey-50 min-h-screen">
-      <h2 className="text-3xl font-semibold">침착맨 굿즈에 로그인하세요.</h2>
+//   return (
+//     <div className="grid grid-cols-[48rem] content-center justify-center gap-[3.2rem] bg-grey-50 min-h-screen">
+//       <h2 className="text-3xl font-semibold">침착맨 굿즈에 로그인하세요.</h2>
 
-      <Form onSubmit={handleSubmit}>
-        {/* <Form> */}
-        {/* <button className="flex items-center gap-6 text-lg border border-primary-300 px-10 py-4 font-medium"> */}
-        <FormRowVertical>
-          <Input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            // disabled={isPending}
-          />
-        </FormRowVertical>
-        <FormRowVertical>
-          <Input
-            type="password"
-            id="password"
-            autoComplete="current-password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            // disabled={isPending}
-          />
-        </FormRowVertical>
-        <FormRowVertical>
-          <Button size="large" disabled={isPending}>
-            {!isPending ? "로그인" : "기다려주세요"}
-          </Button>
-        </FormRowVertical>
-      </Form>
-    </div>
-  );
-}
+//       <Form onSubmit={handleSubmit}>
+//         {/* <Form> */}
+//         {/* <button className="flex items-center gap-6 text-lg border border-primary-300 px-10 py-4 font-medium"> */}
+//         <FormRowVertical>
+//           <Input
+//             type="email"
+//             id="email"
+//             value={email}
+//             onChange={(e) => setEmail(e.target.value)}
+//             // disabled={isPending}
+//           />
+//         </FormRowVertical>
+//         <FormRowVertical>
+//           <Input
+//             type="password"
+//             id="password"
+//             autoComplete="current-password"
+//             value={password}
+//             onChange={(e) => setPassword(e.target.value)}
+//             // disabled={isPending}
+//           />
+//         </FormRowVertical>
+//         <FormRowVertical>
+//           <Button size="large" disabled={isPending}>
+//             {!isPending ? "로그인" : "기다려주세요"}
+//           </Button>
+//         </FormRowVertical>
+//       </Form>
+//     </div>
+//   );
+// }
--- makegoods/src/app/login/useLogin.ts
+++ makegoods/src/app/login/useLogin.ts
@@ -1,30 +1,30 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { login as loginApi } from "../../services/apiAuth";
-// import { useNavigate } from "react-router-dom";
-// import { toast } from "react-hot-toast";
-import { useRouter } from "next/navigation";
+// import { useMutation, useQueryClient } from "@tanstack/react-query";
+// import { login as loginApi } from "../../services/apiAuth";
+// // import { useNavigate } from "react-router-dom";
+// // import { toast } from "react-hot-toast";
+// import { useRouter } from "next/navigation";
 
-export function useLogin() {
-  const queryClient = useQueryClient();
-  const router = useRouter();
+// export function useLogin() {
+//   const queryClient = useQueryClient();
+//   const router = useRouter();
 
-  const { mutate: login, isPending } = useMutation({
-    mutationFn: ({ email, password }) => loginApi({ email, password }),
-    onSuccess: (user) => {
-      queryClient.setQueryData(["user"], user.user);
-      // console.log("QUERY");
-      // const configLocal = localStorage.getItem("configurationId");
+//   const { mutate: login, isPending } = useMutation({
+//     mutationFn: ({ email, password }) => loginApi({ email, password }),
+//     onSuccess: (user) => {
+//       queryClient.setQueryData(["user"], user.user);
+//       // console.log("QUERY");
+//       // const configLocal = localStorage.getItem("configurationId");
 
-      // if (configLocal) {
-      // router.push(`/order/checkout?id=${configLocal}`);
-      // router.push(`/order/checkout?id=${configLocal}`);
-      // }
-      // navigate("/dashboard", { replace: true });
-    },
-    onError: (err) => {
-      // toast.error(`입력된 이메일이나 비밀번호가 틀렸습니다. ${err}`);
-    },
-  });
+//       // if (configLocal) {
+//       // router.push(`/order/checkout?id=${configLocal}`);
+//       // router.push(`/order/checkout?id=${configLocal}`);
+//       // }
+//       // navigate("/dashboard", { replace: true });
+//     },
+//     onError: (err) => {
+//       // toast.error(`입력된 이메일이나 비밀번호가 틀렸습니다. ${err}`);
+//     },
+//   });
 
-  return { login, isPending };
-}
+//   return { login, isPending };
+// }
