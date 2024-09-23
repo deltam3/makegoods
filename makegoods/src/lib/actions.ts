e779077b14f7b7bb2d9d06a9ad6ee925ff8a4b76
deltam3 <practicect@proton.me>
2024-09-23 17:14:02 +0900

    disable actions lib

--- makegoods/src/lib/actions.ts
+++ makegoods/src/lib/actions.ts
@@ -1,15 +1,15 @@
-"use server";
+// "use server";
 
-import { revalidatePath } from "next/cache";
-import { auth, signIn, signOut } from "./auth";
-import { supabase } from "./supabase";
-import { getReservations } from "./data-service";
-import { redirect } from "next/navigation";
+// import { revalidatePath } from "next/cache";
+// import { auth, signIn, signOut } from "./auth";
+// import { supabase } from "./supabase";
+// import { getReservations } from "./data-service";
+// import { redirect } from "next/navigation";
 
-export async function signInAction() {
-  await signIn("google", { redirectTo: "/account" });
-}
+// export async function signInAction() {
+//   await signIn("google", { redirectTo: "/account" });
+// }
 
-export async function signOutAction() {
-  await signOut({ redirectTo: "/" });
-}
+// export async function signOutAction() {
+//   await signOut({ redirectTo: "/" });
+// }
