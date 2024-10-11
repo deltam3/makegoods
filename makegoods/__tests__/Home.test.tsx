a54fc97901dd258ec0df5def9ae50be4f5d03565
deltam3 <practicect@proton.me>
2024-10-12 08:46:46 +0900

    design fix for login and signup and Home.test.tsx

--- makegoods/__tests__/Home.test.tsx
+++ makegoods/__tests__/Home.test.tsx
@@ -5,7 +5,7 @@ describe("Home", () => {
   it("should have h1 text", () => {
     render(<Home />);
 
-    const myElem = screen.getByText("침착맨");
+    const myElem = screen.getByText("굿즈");
 
     expect(myElem).toBeInTheDocument();
   });
