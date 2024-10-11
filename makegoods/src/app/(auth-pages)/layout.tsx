40af68edd36e2e2250c380b8bc1908eef90f1b70
deltam3 <practicect@proton.me>
2024-10-12 08:46:35 +0900

    design fix for login and signup and Home.test.tsx

--- makegoods/src/app/(auth-pages)/layout.tsx
+++ makegoods/src/app/(auth-pages)/layout.tsx
@@ -4,7 +4,7 @@ export default async function Layout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="max-w-7xl flex flex-col gap-12 justify-center">
+    <div className="max-w-7xl flex flex-col gap-12 justify-center mx-auto">
       {children}
     </div>
   );
